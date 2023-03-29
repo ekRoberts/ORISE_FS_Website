ec6b140dc7b30ad7a3f23f4f7748043fe1c41f12
ekRoberts <113919775+ekRoberts@users.noreply.github.com>
2023-03-29 11:54:15 -0600

    Update app.R
    Updated so that the lichen plot works

--- app.R
+++ app.R
@@ -7,12 +7,17 @@ library(ggplot2)
 
 googledownload <- function(id) {
   read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
-  }
+}
 
 #plot_database <- read.csv("~/Desktop/PWS465/shinyapp2/MegaDbPLOT_2022.10.06v2 minus FIA locale.csv", stringsAsFactors = F)
 plot_database <- googledownload('1FTF8Qsl6Ix_V4NTLucC7AenTdGab9Ntg')
 lichen_database <- read.csv("~/Desktop/PWS465/shinyapp2/MegaDbLICHEN_2021.06.03V3.csv", stringsAsFactors = F) 
 elemental_database <- read.csv("~/Desktop/PWS465/shinyapp2/MegaDbELEMENTAL_2021.05.30.csv", stringsAsFactors = F)
+
+
+# plot_database <- read.csv("~/Desktop/PWS465/shinyapp2/MegaDbPLOT_2022.10.06v2 minus FIA locale.csv", stringsAsFactors = F)
+# lichen_database <- read.csv("~/Desktop/PWS465/shinyapp2/MegaDbLICHEN_2021.06.03V3.csv", stringsAsFactors = F) 
+# elemental_database <- read.csv("~/Desktop/PWS465/shinyapp2/MegaDbELEMENTAL_2021.05.30.csv", stringsAsFactors = F)
 
 wilderness_megadbid <- plot_database %>% select(megadbid, wilderns, area)
 
@@ -33,33 +38,33 @@ ui <- fluidPage(
       htmlOutput("national_forest_selector"),
       htmlOutput("wilderness_selector"),
       htmlOutput("variable1_selector")
-
+      
     ),
     mainPanel(
       h3(textOutput("caption")),
       plotOutput("elementalPlot")
     )
-  )
-  downloadButton('downloadPlot', label="Download Plot"),
-  downloadButton('downloadData', label="DownloadButton")
+  ),
+  downloadButton('downloadPlot', label = "Download Plot"),
+  downloadButton('downloadData', label = "Download Dataset")
 )
 
 server <- function(input, output){
   
-#   output$data_selector = renderUI({ #creates State select box object called in ui
-# #default choice (not required)
-#   })
+  #   output$data_selector = renderUI({ #creates State select box object called in ui
+  # #default choice (not required)
+  #   })
   
   data_available <- reactive({
-      if(input$data == "Lichen"){
-        data_active = lichen_database %>% 
-          rename("reg_id" = nfs_reg)
-      }else if(input$data == "Elemental"){
-        data_active = read.csv("~/Desktop/PWS465/shinyapp2/MegaDbELEMENTAL_2021.05.30.csv", stringsAsFactors = F)
-      }else if(input$data == "Plot"){
-        data_active = read.csv("~/Desktop/PWS465/shinyapp2/MegaDbPLOT_2022.10.06v2 minus FIA locale.csv", stringsAsFactors = F)%>% 
-          rename("reg_id" = nfs_reg)
-      } 
+    if(input$data == "Lichen"){
+      data_active = lichen_database %>% 
+        rename("reg_id" = nfs_reg)
+    }else if(input$data == "Elemental"){
+      data_active = read.csv("~/Desktop/PWS465/shinyapp2/MegaDbELEMENTAL_2021.05.30.csv", stringsAsFactors = F)
+    }else if(input$data == "Plot"){
+      data_active = read.csv("~/Desktop/PWS465/shinyapp2/MegaDbPLOT_2022.10.06v2 minus FIA locale.csv", stringsAsFactors = F)%>% 
+        rename("reg_id" = nfs_reg)
+    } 
   })
   
   output$region_selector = renderUI({ #creates State select box object called in ui
@@ -67,18 +72,18 @@ server <- function(input, output){
                 label = "Region:", #label displayed in ui
                 choices = as.character(data_available() %>% pull(reg_id)%>%unique()%>%sort()),
                 # calls unique values from the State column in the previously created table
-                ) #default choice (not required)
+    ) #default choice (not required)
   })
   output$national_forest_selector = renderUI({#creates County select box object called in ui
-
+    
     forest_data = data_available() %>% filter(reg_id == as.integer(input$region))
     #creates a reactive list of available counties based on the State selection made
-
+    
     selectInput(inputId = "national_forest", #name of input
                 label = "National Forest:", #label displayed in ui
                 choices = unique(forest_data$area), #calls list of available counties
                 #selected = unique(data_available)[1]
-                )
+    )
   })
   output$wilderness_selector = renderUI({#creates County select box object called in ui
     wilderness_data = data_available() %>% filter(area == input$national_forest)
@@ -91,25 +96,28 @@ server <- function(input, output){
   })
   
   output$variable1_selector = renderUI({#creates County select box object called in ui
-      variable_data = data_available() %>% filter(wilderns == input$wilderness)
+    variable_data = data_available() %>% filter(wilderns == input$wilderness)
     
     #creates a reactive list of available counties based on the State selection made
     
     selectInput(inputId = "variable1", #name of input
                 label = "Select an Output:", #label displayed in ui
                 choices = c("Histogram", "List of Wilderness species", "List of plots", "Elemental/Year graph"), #calls list of available counties
-                #selected = unique(data_available)[1]
+                selected = "Histogram"
     )
   })
   
   output$elementalPlot = renderPlot({
     variable_data = data_available() %>% filter(wilderns == input$wilderness)
-    
     if(input$variable1 == "Histogram"){
+      #output$plot1 <- renderPlot({
       ggplot(variable_data, aes(x=sci_22chklst))+
         geom_bar()+
         theme(axis.text.x = element_text(angle = 90))+
         labs(x = "Lichen Species", title = "Histogram of Lichen Species within Selected Wilderness")
+        
+      #})
+      #plotOutput("plot1")
       
     }else if(input$variable1 == "List of Wilderness species"){
       output$table <- renderTable(variable_data)
@@ -119,24 +127,24 @@ server <- function(input, output){
       
     }
     
-    ggplot(variable_data, aes_string(x=input$variable1))+
-      geom_bar()+
-      theme(axis.text.x = element_text(angle = 90))
+    # ggplot(variable_data, aes_string(x=input$variable1))+
+    #   geom_bar()+
+    #   theme(axis.text.x = element_text(angle = 90))
   })
-    
+  
   output$downloadPlot <- downloadHandler(
-    filename = 'plot.png',
-    content = function(file) {
-        ggsave(file, plot = output$elementalPlot, device = "png")
-    }
-  )
-  output$downloadData <- downloadHandler(
-   filename = 'dataset.csv',
+   filename = 'plot.png',
    content = function(file) {
-     write.csv(variable_data, file)
+       ggsave(file, plot = "elementalPlot", device = "png")
    }
   )
-
+  output$downloadData <- downloadHandler(
+  filename = 'dataset.csv',
+  content = function(file) {
+    write.csv(variable_data, file)
   }
+  )
+  
+}
 
 shinyApp(ui, server)
